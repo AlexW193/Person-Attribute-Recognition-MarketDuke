20314646686d586a591442ed1dfbd3b99a0e12d4
AlexW193 <alxw630723@gmail.com>
2026-01-21 21:30:37 +0800

    Debug messages added to validate GPU usage if 0 Vram is appararent

--- inference.py
+++ inference.py
@@ -9,6 +9,12 @@ from net import get_model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
+print("torch version:", torch.__version__)
+print("cuda available:", torch.cuda.is_available())
+print("cuda device count:", torch.cuda.device_count())
+if torch.cuda.is_available():
+    print("cuda device name:", torch.cuda.get_device_name(0))
+
 ######################################################################
 # Settings
 # ---------
